58ef32abff26da055228cfda5637164f9f881453
Clément E <clement.eraud@lonestone.studio>
2021-08-25 12:03:57 +0200

    Update the front accordingly to the refactored demo server

--- demo/front/src/App.tsx
+++ demo/front/src/App.tsx
@@ -1,21 +1,21 @@
 import { h } from 'preact'
 import { useEffect, useState } from 'preact/hooks'
 import { articleController } from './sdk/articleModule'
+import { authorController } from './sdk/authorModule'
 import { categoryController } from './sdk/categoryModule'
-import { userController } from './sdk/userModule'
 import { Article } from './sdk/_types/modules/article/article.entity'
+import { Author } from './sdk/_types/modules/author/author.entity'
 import { Category } from './sdk/_types/modules/category/category.entity'
-import { User } from './sdk/_types/modules/user/user.entity'
 
 export function App() {
-  const [error, setError] = useState<string | null>(null)
+  const [error, setError] = useState(null)
 
-  const [users, setUsers] = useState<User[]>([])
+  const [authors, setAuthors] = useState<Author[]>([])
   const [categories, setCategories] = useState<Category[]>([])
   const [articles, setArticles] = useState<Article[]>([])
 
   useEffect(() => {
-    userController.getAll().then(setUsers, setError)
+    authorController.getAll().then(setAuthors, setError)
   }, [])
 
   useEffect(() => {
@@ -32,24 +32,24 @@ export function App() {
     <div>
       <h1>NSdkGen Demo App</h1>
 
-      <h2>Users</h2>
+      <h2>Authors</h2>
       <ol>
-        {users.map((user) => (
-          <li key={user.uuid}>{user.displayName}</li>
+        {authors.map((author) => (
+          <li key={author.id}>{author.displayName}</li>
         ))}
       </ol>
 
       <h2>Categories</h2>
       <ol>
         {categories.map((category) => (
-          <li key={category.uuid}>{category.title}</li>
+          <li key={category.id}>{category.title}</li>
         ))}
       </ol>
 
       <h2>Articles</h2>
       <ol>
         {articles.map((article) => (
-          <li key={article.uuid}>{article.title}</li>
+          <li key={article.id}>{article.title}</li>
         ))}
       </ol>
     </div>
