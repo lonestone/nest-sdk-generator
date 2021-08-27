c01e4b355e2780895bbda32af3936ac4e7ee041b
Clément E <clement.eraud@lonestone.studio>
2021-08-27 15:16:10 +0200

    Fix: Content-Type when making requests to Nest.js API

--- src/generator/sdk-interface.ts
+++ src/generator/sdk-interface.ts
@@ -3,17 +3,30 @@ export async function request(
   method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE',
   uri: string,
   query: Record<string, string>,
-  body: unknown
+  body: unknown,
 ): Promise<any> {
   const url = new URL('http://localhost:3000' + uri)
   url.search = new URLSearchParams(query).toString()
 
-  const params: RequestInit = { method }
+  const params: RequestInit = {
+    method,
+    // Required for content to be correctly parsed by Nest.js
+    headers: { 'Content-Type': 'application/json' },
+  }
 
+  // Setting a body is forbidden on GET requests
   if (method !== 'GET') {
     params.body = JSON.stringify(body)
   }
 
-  return fetch(url.toString(), params).then((res) => res.json())
+  return fetch(url.toString(), params).then((res) => {
+    // Handle failed requests
+    if (!res.ok) {
+      throw Error(res.statusText)
+    }
+
+    return res.json()
+  })
 }
+
 `.trim()
