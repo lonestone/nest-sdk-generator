a6754e0eed64987e8ff5fd3e913f7776e756843f
Clément E <clement.eraud@lonestone.studio>
2021-08-27 18:22:17 +0200

    Fix: find Prettier configuration correctly

--- src/generator/prettier.ts
+++ src/generator/prettier.ts
@@ -9,14 +9,20 @@ import { findFileAbove } from '../file-utils'
 import { panic } from '../logging'
 
 /**
- * Find a .prettier.rc configuration file in the current directory or above
+ * Find a .prettierrc configuration file in the current directory or above
  */
 export function findPrettierConfig(config: Config): object {
-  const text = config.prettierConfig
-    ? fs.existsSync(config.prettierConfig)
-      ? fs.readFileSync(config.prettierConfig, 'utf8')
-      : panic('Prettier configuration was not found at specified path {magenta}', config.prettierConfig)
-    : findFileAbove('.prettier.rc', config.sdkOutput) ?? '{}'
+  let prettierConfigPath = config.prettierConfig ?? findFileAbove('.prettierrc', config.sdkOutput)
+
+  if (!prettierConfigPath) {
+    return {}
+  }
+
+  if (!fs.existsSync(prettierConfigPath)) {
+    panic('Prettier configuration was not found at specified path {magenta}', prettierConfigPath)
+  }
+
+  const text = fs.readFileSync(prettierConfigPath, 'utf8')
 
   try {
     return JSON.parse(text)
