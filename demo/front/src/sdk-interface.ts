7b9b7e1e727180404f3dd0728d4ae1eb6b8c18bb
Clément E <clement.eraud@lonestone.studio>
2021-08-27 15:18:10 +0200

    Regenerate frontend's SDK interface

--- demo/front/src/sdk-interface.ts
+++ demo/front/src/sdk-interface.ts
@@ -7,11 +7,23 @@ export async function request(
   const url = new URL('http://localhost:3000' + uri)
   url.search = new URLSearchParams(query).toString()
 
-  const params: RequestInit = { method }
+  const params: RequestInit = {
+    method,
+    // Required for content to be correctly parsed by Nest.js
+    headers: { 'Content-Type': 'application/json' },
+  }
 
+  // Setting a body is forbidden on GET requests
   if (method !== 'GET') {
     params.body = JSON.stringify(body)
   }
 
-  return fetch(url.toString(), params).then((res) => res.json())
+  return fetch(url.toString(), params).then((res) => {
+    // Handle failed requests
+    if (!res.ok) {
+      throw Error(res.statusText)
+    }
+
+    return res.json()
+  })
 }
