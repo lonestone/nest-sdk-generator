664c4fd6b9b673a9d3d0be878e0cc3720b4ef42c
Clément E <clement.eraud@lonestone.studio>
2021-08-30 11:47:38 +0200

    Add comments on the complex type generator

--- src/generator/gentypes.ts
+++ src/generator/gentypes.ts
@@ -6,32 +6,48 @@ import * as path from 'path'
 import { TypesExtractorContent } from '../analyzer/extractor'
 import { normalizeExternalFilePath } from '../analyzer/typedeps'
 
-// Returned codes are not formatted yet
+/**
+ * Generate the non-formatted type files which will be used by the SDK's route functions
+ * @param sdkTypes
+ * @returns
+ */
 export function generateSdkTypeFiles(sdkTypes: TypesExtractorContent): Map<string, string> {
+  /** Generated type files */
   const genFiles = new Map<string, string>()
 
+  // While analyzing the controllers' content, all types used by their methods have been extracted
+  // The list of these types is then provided to this function as the `sdkTypes` argument, allowing us
+  //  to establish the list of all dependencies.
   for (const [file, types] of sdkTypes) {
     const out = []
 
+    /** List of imports in the current file */
     const imports = new Map<string, string[]>()
 
+    // Iterate over all types in the current `file`
     for (const extracted of types.values()) {
+      // Iterate over all of the extracted type's dependencies
       for (const dep of extracted.dependencies) {
-        if (dep.relativePath !== file) {
-          let imported = imports.get(dep.relativePathNoExt)
+        // If the dependency is from the same file, then we have nothing to do
+        if (dep.relativePath === file) {
+          continue
+        }
 
-          if (!imported) {
-            imported = [dep.typename]
-            imports.set(dep.relativePathNoExt, imported)
-          }
+        // Push the typename to the list of imports
+        let imported = imports.get(dep.relativePathNoExt)
 
-          if (!imported.includes(dep.typename)) {
-            imported.push(dep.typename)
-          }
+        if (!imported) {
+          imported = [dep.typename]
+          imports.set(dep.relativePathNoExt, imported)
+        }
+
+        if (!imported.includes(dep.typename)) {
+          imported.push(dep.typename)
         }
       }
     }
 
+    // Generate an import statement for each imported type
     out.push(
       [...imports]
         .map(([depFile, types]) => {
@@ -44,10 +60,12 @@ export function generateSdkTypeFiles(sdkTypes: TypesExtractorContent): Map<strin
         .join('\n')
     )
 
+    // Add the extracted types' declaration, indented
     for (const extracted of types.values()) {
       out.push(extracted.content.replace(/^/gm, '  '))
     }
 
+    // Generate the type file
     genFiles.set(file, out.join('\n'))
   }
 
