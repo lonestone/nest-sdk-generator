90b1fd2b0429e9e314672065c39dc64dd48accfe
Clément E <clement.eraud@lonestone.studio>
2022-03-04 14:04:09 +0100

    Add JSDoc on the configuration interface

--- src/config.ts
+++ src/config.ts
@@ -7,21 +7,43 @@ import * as path from 'path'
  * For details on what these options do, see the project's README
  */
 export interface Config {
+  /** Enable verbose mode */
   readonly verbose?: boolean
+
+  /** Disable colored output */
   readonly noColor?: boolean
 
+  /** Path to the API's source directory */
   readonly apiInputPath: string
+
+  /** Path to generate the SDK at */
   readonly sdkOutput: string
+
+  /** Path to the SDK interface file */
   readonly sdkInterfacePath: string
 
+  /** List of magic types */
   readonly magicTypes?: MagicType[]
+
+  /** Show a JSON output */
   readonly jsonOutput?: string
+
+  /** Prettify the JSON output */
   readonly jsonPrettyOutput?: boolean
+
+  /** Prettify the generated files (enabled by default) */
+  readonly prettify?: boolean
+
+  /** Path to Prettier's configuration file */
   readonly prettierConfig?: string
 
-  readonly prettify?: boolean
+  /** If the output directory already exists, overwrite it (enabled by default) */
   readonly overwriteOldOutputDir?: boolean
+
+  /** If the SDK interface file does not exist yet, create one automatically (enabled by default) */
   readonly generateDefaultSdkInterface?: boolean
+
+  /** Write generation timestamp in each TypeScript file (enabled by default) */
   readonly generateTimestamps?: boolean
 }
 
